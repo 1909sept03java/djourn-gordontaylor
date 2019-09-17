d20f1ada8376e79cc229f71e7e3f03fe18301559
Djourn G.A. Gordon-Taylor Soyer <dsoyer12@gmail.com>
2019-09-17 14:12:08 -0400

    posted HTML assignment

---
+++ HTML/dev/scripts/app.js
@@ -0,0 +1,15 @@
+/*jslint browser, es6 */
+/*global window */
+
+window.onload = function () {
+    const CSSPreProcessor = `Sass`;
+    const JSVersion = `ES6`;
+
+   
+};
+
+function changeFontColor() {
+    var thisColor = document.getElementById("currentColor");
+    var newColor = document.getElementById("newColor").value;
+    thisColor.style.color = newColor;
+  }
