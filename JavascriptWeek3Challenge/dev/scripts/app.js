88d0698e319a66b0aa778f1eb78f3bf335f2a0ef
Djourn G.A. Gordon-Taylor Soyer <dsoyer12@gmail.com>
2019-09-23 13:57:01 -0400

    uploaded week 3 coding challenge

---
+++ JavascriptWeek3Challenge/dev/scripts/app.js
@@ -0,0 +1,56 @@
+/*jslint browser, es6 */
+/*global window */
+
+window.onload = function() {
+    const CSSPreProcessor = `Sass`;
+    const JSVersion = `ES6`;
+
+
+};
+
+
+var url = "https://randomuser.me/api/?results=25";
+var p = "";
+
+
+
+document.getElementById('getText').addEventListener('click', getText);
+fetch(url)
+    .then((response) => response.json())
+    .then(function(data) {
+        let url = 'https://randomuser.me/api/?results=25';
+        fetchInformation(url);
+
+    })
+
+function getText() {
+    fetch(url)
+        .then((response) => response.json())
+        .then(function(data) {
+
+            data.results.forEach(person => {
+
+                p = `<div class="card" style="color:black; background-color:darkgray;">
+           <img  class = "giphy-embed"src="${person.picture.medium}"  alt="Bio img">
+           <ul>
+           <li>${person.name.title} ${person.name.first} ${person.name.last}</li>
+           <li>
+           Nationality:${person.nat}</li>
+           <li>
+           Address:${person.location.street},${person.location.city},${person.location.state}</li>
+           <li>Abilities: Classified </li>
+           
+           </ul>
+            
+           
+           </div>`;
+                $("#people").html(p);
+                // $("#result").append(p);
+
+            });
+
+
+
+        })
+
+}
